aa9f6398609c82d858e4efdbab22ced7d224a45c
Crossle Song <crosslesong@gmail.com>
2019-10-22 10:50:32 +0800

    RSA key use 2048 length

--- src/mixin.js
+++ src/mixin.js
@@ -9,18 +9,18 @@ function Mixin() {
 }
 
 Mixin.prototype = {
-  generateSessionKeypair: function() {
-    let keypair = forge.pki.rsa.generateKeyPair({bits: 1024, e: 0x10001});
+  generateSessionKeypair: function () {
+    let keypair = forge.pki.rsa.generateKeyPair({ bits: 2048, e: 0x10001 });
     let body = forge.asn1.toDer(forge.pki.publicKeyToAsn1(keypair.publicKey)).getBytes();
     let public_key = forge.util.encode64(body, 64);
     let private_key = forge.pki.privateKeyToPem(keypair.privateKey);
-    return {public: public_key, private: private_key}
+    return { public: public_key, private: private_key }
   },
 
   signAuthenticationToken: function (uid, sid, privateKey, method, uri, params, scp) {
-    if (typeof(params) === "object") {
+    if (typeof (params) === "object") {
       params = JSON.stringify(params);
-    } else if (typeof(params) !== "string") {
+    } else if (typeof (params) !== "string") {
       params = ""
     }
 
@@ -30,13 +30,13 @@ Mixin.prototype = {
     let payload = {
       uid: uid,
       sid: sid,
-      iat: moment.utc().unix() ,
+      iat: moment.utc().unix(),
       exp: expire,
       jti: uuid(),
       sig: md.digest().toHex(),
       scp: scp
     };
-    return jwt.sign(payload, privateKey, { algorithm: 'RS512'});
+    return jwt.sign(payload, privateKey, { algorithm: 'RS512' });
   },
 
   signEncryptedPin: function (pin, pinToken, sessionId, privateKey, iterator) {
@@ -60,7 +60,7 @@ Mixin.prototype = {
     let buf = Buffer.concat([pin, Buffer.from(time), Buffer.from(iterator)]);
     let padding = blockSize - buf.length % blockSize;
     let paddingArray = [];
-    for (let i=0; i<padding; i++) {
+    for (let i = 0; i < padding; i++) {
       paddingArray.push(padding);
     }
     buf = Buffer.concat([buf, new Buffer(paddingArray)]);
@@ -68,17 +68,17 @@ Mixin.prototype = {
     let iv16 = forge.random.getBytesSync(16);
     let key = this.hexToBytes(forge.util.binary.hex.encode(pinKey));
     let cipher = forge.cipher.createCipher('AES-CBC', key);
-    cipher.start({iv: iv16});
+    cipher.start({ iv: iv16 });
     cipher.update(buf)
     cipher.finish();
     let encrypted_pin_buff = cipher.output;
-    encrypted_pin_buff = Buffer.concat([iv16 , encrypted_pin_buff]);
+    encrypted_pin_buff = Buffer.concat([iv16, encrypted_pin_buff]);
     return Buffer.from(encrypted_pin_buff).toString('base64');
   },
 
   hexToBytes: function (hex) {
     var bytes = [];
-    for (let c=0; c<hex.length; c+=2) {
+    for (let c = 0; c < hex.length; c += 2) {
       bytes.push(parseInt(hex.substr(c, 2), 16));
     }
     return bytes;
