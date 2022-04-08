b9f134e1fd582a2857d7b17c1ef06bda209461df
LiYuqing <im.yuqlee@gmail.com>
2022-04-08 11:03:43 +0800

    Code Refactor: tests should in independent dir

--- tests/client.test.js
+++ tests/client.test.js
@@ -1,4 +1,4 @@
-import Client from './client'
+import Client from '../src/client'
 
 const client = new Client();
 
--- tests/user.test.js
+++ tests/user.test.js
@@ -1,4 +1,4 @@
-import User from './user'
+import User from '../src/user'
 
 const user = new User();
 const appId = '';
