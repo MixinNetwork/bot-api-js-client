605eb53ca482554e08069578197a33d295dd7614
LiYuqing <im.yuqlee@gmail.com>
2022-04-13 17:20:23 +0800

    Add transfer apis with tests

--- src/mixin.js
+++ src/mixin.js
@@ -1,9 +1,11 @@
+import Transfer from './transfer';
 import User from './user';
 import HTTP from './http';
 import Utils from './utils';
 
 class Mixin {
   constructor(keystore) {
+    this.transfer = new Transfer(keystore);
     this.user = new User(keystore);
     this.http = new HTTP(keystore);
     this.utils = Utils;
