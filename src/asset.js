cd6c87e7fac2ea450a6ec93f56776850ef8ea7e8
HunterQ <im.yuqlee@gmail.com>
2022-04-14 11:50:12 +0800

    Feature/oauth (#193)
    * Add oauth
    
    * Some comments of the asset
    
    * Fix text

--- src/asset.js
+++ src/asset.js
@@ -6,14 +6,19 @@ class Asset {
     this.http = new HTTP(this.keystore);
   }
 
+  // List the user's assets
+  // For messenger user and app user, all chain assets will be listed, even they don't have.
+  // For network user, assets balance greater than 0 will be listed only.
   Index() {
     return this.http.request('GET', '/assets');
   }
 
+  // List the information of users asset, if user don't have the address of the asset, it will create automatically.
   Show(id) {
     return this.http.request('GET', `/assets/${id}`);
   }
 
+  // List the transactions of the asset.
   Snapshots(id) {
     return this.http.request('GET', `/assets/${id}/snapshots`);
   }
---
+++ src/oauth.js
@@ -0,0 +1,38 @@
+import HTTP from './http';
+
+class OAuth {
+  constructor(keystore) {
+    this.keystore = keystore || {};
+    this.http = new HTTP(this.keystore);
+  }
+
+  // List the user's authorizations
+  // an optional params "app", which specify the app id
+  // will return an array of authorizations or the authorization of the app
+  Authorizations() {
+    return this.http.request('GET', '/authorizations');
+  }
+
+  // body: { "authorization_id": UUID, scopes: [] }
+  // user can authorize authorizations for the app.
+  Authorize(body) {
+    return this.http.request('POST', '/oauth/authorize', body);
+  }
+
+  // body: { "client_id": UUID, "code": "", "client_secret": "", "code_verifier": "", "ed25519": "" }
+  // After get `authorization_code` from `/oauth/authorize`, you can get the `access_token` from this api.
+  // `ed25519` is optional
+  // 1. Without `ed25519`, will response an `access_token`, which will expired in 365 days.
+  // 2. Recommendation with `ed25519`, you need sign the `authorization_token` every time, but the `access_token`
+  //    never expired.
+  Token(body) {
+    return this.http.request('POST', '/oauth/token', body);
+  }
+
+  // Since some app is danger or you don't want the app have access of your information, you can cancel it, through the api.
+  Cancel(body) {
+    return this.http.request('POST', '/oauth/cancel', body);
+  }
+}
+
+export default OAuth;
